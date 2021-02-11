3740d7ff36a4d0062369bb1ece2a930e5b3ceb74
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-02-11 10:38:28 +0100

    initial commit

---
+++ src/main/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule_JWT_Client/SpringAcademySpringSecurityModuleJwtClientApplication.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule_JWT_Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademySpringSecurityModuleJwtClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademySpringSecurityModuleJwtClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/warkoczewski/SpringAcademy_SpringSecurityModule_JWT_Client/SpringAcademySpringSecurityModuleJwtClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.SpringAcademy_SpringSecurityModule_JWT_Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademySpringSecurityModuleJwtClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
